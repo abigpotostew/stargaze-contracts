dc23c7c09cbfaace0fa0ace495f8527065906197
Stewart <stew.bracken@gmail.com>
2023-02-20 20:30:56 -0800

    Update optimize.sh

--- scripts/optimize.sh
+++ scripts/optimize.sh
@@ -1,4 +1,5 @@
 docker run --rm -v "$(pwd)":/code --platform linux/amd64 \
 	--mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
 	--mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+  --env RUST_BACKTRACE=1 \
 	cosmwasm/workspace-optimizer:0.12.7
